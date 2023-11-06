32133d300642e6acb112bb606b8cde65c96415e0
Oleg Golovkov <oleg.golovkov@applifting.cz>
2023-11-06 11:15:10 +0100

    added textStyle and colors

--- app/src/main/java/eu/golovkov/ackeeram/ui/theme/Color.kt
+++ app/src/main/java/eu/golovkov/ackeeram/ui/theme/Color.kt
@@ -2,10 +2,40 @@ package eu.golovkov.ackeeram.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//Light Mode
+//Accent
+val accentPrimary = Color(0xFF0000FF)
+//Backgrounds
+val backgroundsPrimary = Color(0xFFF4F4F9)
+val backgroundsSecondary = Color(0xFFE7E7EC)
+val backgroundsTertiary = Color(0xFFFFFFFF)
+val backgroundsBottomNav = Color(0xFFFFFFFF)
+//Foregrounds
+val foregroundsPrimary = Color(0xFF000000)
+val foregroundsSecondary = Color(0xFF666666)
+val foregroundsTertiary = Color(0xFFB3B3B3)
+val foregroundsSeparator = Color(0xFFF0F0F0)
+val foregroundsOnPrimary = Color(0xFFF0F0F0)
+//Icons
+val iconsPrimary = Color(0xFF000000) // same as foregroundsPrimary
+val iconsSecondary = Color(0xFFB3B3B3) // same as foregroundsTertiary
+val iconsTertiary = Color(0xFF0000FF) // same as accentPrimary
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+//Dark Mode
+//Accent
+val accentPrimaryDm = Color(0xFF9595FE)
+//Backgrounds
+val backgroundsPrimaryDm = Color(0xFF181819)
+val backgroundsSecondaryDm = Color(0xFF2E2E2F)
+val backgroundsTertiaryDm = Color(0xFF474747)
+val backgroundsBottomNavDm = Color(0xFF0F0F10)
+//Foregrounds
+val foregroundsPrimaryDm = Color(0xFFFFFFFF)
+val foregroundsSecondaryDm = Color(0xFF8B8B8C)
+val foregroundsTertiaryDm = Color(0xFF5D5D5E)
+val foregroundsSeparatorDm = Color(0xFF474749)
+val foregroundsOnPrimaryDm = Color(0xFF000000)
+//Icons
+val iconsPrimaryDm = Color(0xFFFFFFFF) // same as foregroundsPrimaryDm
+val iconsSecondaryDm = Color(0xFFB3B3B3)
+val iconsTertiaryDm = Color(0xFF9595FE) // same as accentPrimaryDm
--- app/src/main/java/eu/golovkov/ackeeram/ui/theme/Type.kt
+++ app/src/main/java/eu/golovkov/ackeeram/ui/theme/Type.kt
@@ -8,27 +8,46 @@ import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
+    displayLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.sp
+    ),
+    displayMedium = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
+        lineHeight = 20.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
+    displaySmall = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 18.sp,
+        letterSpacing = 0.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 18.sp,
+        letterSpacing = 0.sp
+    ),
 )
